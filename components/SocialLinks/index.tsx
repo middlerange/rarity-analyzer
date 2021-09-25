8d5a9e434b98fc930a658f0a0d068f84708f1604
0xNight <night9uy@gmail.com>
2021-09-25 11:00:03 +0530

    chore: conditional social link

--- components/SocialLinks/index.tsx
+++ components/SocialLinks/index.tsx
@@ -1,54 +1,62 @@
-import * as React from "react";
+import * as React from 'react';
 import {
-	DiscordIcon,
-	InstagramIcon,
-	TwitterIcon,
-	WebsiteLinkIcon,
-} from "../Icons";
+  DiscordIcon,
+  InstagramIcon,
+  TwitterIcon,
+  WebsiteLinkIcon
+} from '../Icons';
 
 const SocialLinks = () => {
-	return (
-		<div className="mt-2">
-			<ul className="flex space-x-5">
-				<li>
-					<a
-						href={process.env.NEXT_PUBLIC_WEBSITE_URL}
-						className="text-gray-500 hover:text-gray-500"
-					>
-						<span className="sr-only">Website</span>
-						<WebsiteLinkIcon />
-					</a>
-				</li>
-				<li>
-					<a
-						href={process.env.NEXT_PUBLIC_DISCORD}
-						className="text-gray-500 hover:text-gray-500"
-					>
-						<span className="sr-only">Discord</span>
-						<DiscordIcon />
-					</a>
-				</li>
-				<li>
-					<a
-						href={process.env.NEXT_PUBLIC_TWITTER}
-						className="text-gray-500 hover:text-gray-500"
-					>
-						<span className="sr-only">Twitter</span>
-						<TwitterIcon />
-					</a>
-				</li>
-				<li>
-					<a
-						href={process.env.NEXT_PUBLIC_INSTAGRAM}
-						className="text-gray-500 hover:text-gray-500"
-					>
-						<span className="sr-only">Twitter</span>
-						<InstagramIcon />
-					</a>
-				</li>
-			</ul>
-		</div>
-	);
+  return (
+    <div className="mt-2">
+      <ul className="flex space-x-5">
+        {process.env.NEXT_PUBLIC_WEBSITE_URL && (
+          <li>
+            <a
+              href={process.env.NEXT_PUBLIC_WEBSITE_URL}
+              className="text-gray-500 hover:text-gray-500"
+            >
+              <span className="sr-only">Website</span>
+              <WebsiteLinkIcon />
+            </a>
+          </li>
+        )}
+        {process.env.NEXT_PUBLIC_DISCORD && (
+          <li>
+            <a
+              href={process.env.NEXT_PUBLIC_DISCORD}
+              className="text-gray-500 hover:text-gray-500"
+            >
+              <span className="sr-only">Discord</span>
+              <DiscordIcon />
+            </a>
+          </li>
+        )}
+        {process.env.NEXT_PUBLIC_TWITTER && (
+          <li>
+            <a
+              href={process.env.NEXT_PUBLIC_TWITTER}
+              className="text-gray-500 hover:text-gray-500"
+            >
+              <span className="sr-only">Twitter</span>
+              <TwitterIcon />
+            </a>
+          </li>
+        )}
+        {process.env.NEXT_PUBLIC_INSTAGRAM && (
+          <li>
+            <a
+              href={process.env.NEXT_PUBLIC_INSTAGRAM}
+              className="text-gray-500 hover:text-gray-500"
+            >
+              <span className="sr-only">Twitter</span>
+              <InstagramIcon />
+            </a>
+          </li>
+        )}
+      </ul>
+    </div>
+  );
 };
 
 export default SocialLinks;
